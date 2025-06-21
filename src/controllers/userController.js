da3357de7210e18fea42ffa8afb6d9057fe2c35e
liorga <liorgal31892@gmail.com>
2025-06-21 19:32:27 +0300

    added api fields missing when register user and if user already exists

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,9 +2,18 @@ const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
-exports.registerUser = async (req, res) => {
+async function registerUser(req, res) {
   try {
     const { name, email, password } = req.body;
+    if (!name || !email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+    // Check if user already exists
+    const existingUser = await User.find({ email });
+    if (existingUser.length > 0) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
     const hashed = await bcrypt.hash(password, 10);
     const user = new User({
       name,
@@ -19,4 +28,5 @@ exports.registerUser = async (req, res) => {
   } catch (error) {
     res.status(500).json({ message: "Server error" });
   }
-};
+}
+exports.registerUser = registerUser;
