dffd61bd9ecd1bbd66db14878f0717030f1d417c
liorga <liorgal31892@gmail.com>
2025-06-21 20:21:02 +0300

    added basic user login route

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -29,4 +29,30 @@ async function registerUser(req, res) {
     res.status(500).json({ message: "Server error" });
   }
 }
-exports.registerUser = registerUser;
+
+async function loginUser(req, res) {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+    // Check if user exists
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "invalid username or password" });
+    }
+    // Check password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "invalid password" });
+    }
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+    res.status(200).json(token);
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+}
+
+module.exports = { registerUser, loginUser };
